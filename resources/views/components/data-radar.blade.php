cedb949692802551f960913811b6e46a3e28e9c7
masif088 <mokhamadasif@gmail.com>
2022-03-30 11:33:06 +0700

    Radar

--- resources/views/components/data-radar.blade.php
+++ resources/views/components/data-radar.blade.php
@@ -39,13 +39,8 @@
         var borderColor = ['#FAA255', '#F0C348', '#E27CF1', '#F562AC', '#EB5959', '#9EE67A', '#50D989', '#66CFF2', '#7F7CE6'];
         borderColor = shuffle(borderColor);
         var options1 = {
-            plugins: {
-                legend: {
-                    display: false,
-                    labels: {
-                        color: 'rgb(255, 99, 132)'
-                    }
-                }
+            legend: {
+                show: true,
             },
             chart: {
                 height: 380,
