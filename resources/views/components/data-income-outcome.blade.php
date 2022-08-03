ffe3125316276a181685fc94a64460b2015164ea
masif088 <mokhamadasif@gmail.com>
2022-08-03 14:05:02 +0700

    produk

--- resources/views/components/data-income-outcome.blade.php
+++ resources/views/components/data-income-outcome.blade.php
@@ -13,9 +13,9 @@
 'data1' => [],
 'dataTitle1' => 'no title',
 'data2' => [],
-'dataTitle2' => 'no title',
+'dataTitle2',
 'data3' => [],
-'dataTitle3' => 'no title',
+'dataTitle3',
 'categories' => [],
 ])
 <div class="col-xl-12 xl-100 dashboard-sec box-col-12">
@@ -25,31 +25,31 @@
                 <div class="col-xl-3 earning-content p-0">
                     <div class="row m-0 chart-left">
                         @isset($title)
-                        <div class="col-xl-12 p-0 left_side_earning">
-                            <h5>DATA</h5>
-                            <p class="font-roboto">{{ $title }}</p>
-                        </div>
+                            <div class="col-xl-12 p-0 left_side_earning">
+                                <h5>DATA</h5>
+                                <p class="font-roboto">{{ $title }}</p>
+                            </div>
                         @endif
                         @isset($title1)
-                        <div class="col-xl-12 p-0 left_side_earning">
-                            <h5>{{ $value1 }}</h5>
-                            <p class="font-roboto">{{ $title1 }}</p>
-                        </div>
+                            <div class="col-xl-12 p-0 left_side_earning">
+                                <h5>{{ $value1 }}</h5>
+                                <p class="font-roboto">{{ $title1 }}</p>
+                            </div>
                         @endif
                         @isset($title2)
-                        <div class="col-xl-12 p-0 left_side_earning">
-                            <h5>{{ $value2 }}</h5>
-                            <p class="font-roboto">{{$title2}}</p>
-                        </div>
+                            <div class="col-xl-12 p-0 left_side_earning">
+                                <h5>{{ $value2 }}</h5>
+                                <p class="font-roboto">{{$title2}}</p>
+                            </div>
                         @endisset
                         @isset($title3)
-                        <div class="col-xl-12 p-0 left_side_earning">
-                            <h5>{{ $value3 }}</h5>
-                            <p class="font-roboto">{{ $title3 }}</p>
-                        </div>
+                            <div class="col-xl-12 p-0 left_side_earning">
+                                <h5>{{ $value3 }}</h5>
+                                <p class="font-roboto">{{ $title3 }}</p>
+                            </div>
                         @endif
-{{--                        <div class="col-xl-6 p-0 left-btn"><a href="{{$link1}}" class="btn {{$btnColor1}}">{{$btn1}}</a></div>--}}
-{{--                        <div class="col-xl-6 p-0 left-btn"><a href="{{$link2}}" class="btn {{$btnColor2}}">{{$btn2}}</a></div>--}}
+                        {{--                        <div class="col-xl-6 p-0 left-btn"><a href="{{$link1}}" class="btn {{$btnColor1}}">{{$btn1}}</a></div>--}}
+                        {{--                        <div class="col-xl-6 p-0 left-btn"><a href="{{$link2}}" class="btn {{$btnColor2}}">{{$btn2}}</a></div>--}}
                     </div>
                 </div>
                 <div class="col-xl-9 p-0">
@@ -97,24 +97,27 @@
                 name: '{{ $dataTitle1 }}',
                 data: [
                     @foreach($data1 as $d1)
-                    {{$d1}},
+                        {{$d1}},
                     @endforeach
                 ]
-            }, {
+            }, @isset($dataTitle2)
+            {
                 name: '{{ $dataTitle2 }}',
                 data: [
                     @foreach($data2 as $d2)
                         {{$d2}},
                     @endforeach
                 ]
-            }, {
+            } @endif , @isset($dataTitle3)
+            {
                 name: '{{ $dataTitle3 }}',
                 data: [
                     @foreach($data3 as $d3)
                         {{$d3}},
                     @endforeach
                 ]
-            }],
+            }@endif
+            ],
             chart: {
                 height: 320,
                 type: 'area',
@@ -153,7 +156,7 @@
             markers: {
                 strokeWidth: 3,
                 colors: "#ffffff",
-                strokeColors: [CubaAdminConfig.primary, CubaAdminConfig.secondary,'#00ff00'],
+                strokeColors: [CubaAdminConfig.primary, CubaAdminConfig.secondary, '#00ff00'],
                 hover: {
                     size: 6,
                 }
@@ -183,7 +186,7 @@
                     top: -40
                 }
             },
-            colors: [CubaAdminConfig.primary, CubaAdminConfig.secondary,'#00ff00'],
+            colors: [CubaAdminConfig.primary, CubaAdminConfig.secondary, '#00ff00'],
             fill: {
                 type: 'gradient',
                 gradient: {
@@ -194,7 +197,7 @@
                 }
             },
             legend: {
-                show: false,
+                show: true,
             },
             tooltip: {
                 x: {
