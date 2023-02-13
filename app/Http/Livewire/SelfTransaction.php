b07bafe6d73502e8985a84d6047da78587ea1156
masif088 <mokhamadasif@gmail.com>
2023-02-13 09:38:57 +0700

    add self transaction

--- app/Http/Livewire/SelfTransaction.php
+++ app/Http/Livewire/SelfTransaction.php
@@ -49,7 +49,7 @@ class SelfTransaction extends Component
         $this->optionMethod = eloquent_to_options(PaymentMethod::get(), 'id',
             'title',);
         $this->products = Product::where('product_status_id', 1)
-            ->orderByRaw("FIELD(product_type_id,15,5,10,11,8,9,12,13,14,21,18,19,17) ASC")
+            ->orderByRaw("FIELD(product_type_id,15,5,10,11,8,9,12,13,14,21,18,19,17) desc")
             ->get();
         $this->productSold = Product::where('product_status_id', 3)
             ->orderBy('product_type_id',)->get();
